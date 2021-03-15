5987c2549e1ce12b3ff3643f13703c8818da585c
Renee Thomsen <chuhan.thomsen@collabera.com>
2021-03-14 22:02:08 -0400

    updated components

---
+++ hello-world/src/app/courses.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'courses',  // <div class="courses"> in css  selector: ".courses" or "#courses"
+//    template: '<h2>{{"Title: " + title}}</h2>'
+//    template: '<h2>{{ getTitle() }}</h2>'
+      template: `
+        <h2> {{title}}</h2>
+        <ul>
+            <li *ngFor="let course of courses">
+                {{course}}
+            </li> 
+        </ul>
+      `
+})
+export class CoursesComponent {
+    title = " List of courses";
+     
+    //method
+    getTitle() {
+        return this.title;
+    }
+
+    courses = ["course1", "course2", "course3"]
+}
