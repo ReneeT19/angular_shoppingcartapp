e60d39e621cf3ae3ec1389189d7fdb52523ecf11
Renee Thomsen <rcthomsen19@gmail.com>
2021-04-06 11:27:07 -0400

    updates

--- hello-world/src/app/app.component.ts
+++ hello-world/src/app/app.component.ts
@@ -27,5 +27,37 @@ export class AppComponent {
   courses= [1,2];
 
   viewMode = 'map';
-  
+
+  courses2 = [
+    {id: 1, name: 'course1'},
+    {id: 2, name: 'course2'},
+    {id: 3, name: 'course3'},
+  ];
+
+  courses3;
+
+  onAdd() {
+    this.courses2.push({ id: 4, name: 'course4'});
+  }
+
+  onRemove(course) {
+    let index =  this.courses2.indexOf(course);
+    this.courses2.splice(index,1);
+  }
+
+  onChange(course) {
+    course.name = 'UPDATED';
+  }
+
+  loadCourses() {
+    this.courses3 = [
+      {id: 1001, name: 'course1001'},
+      {id: 1002, name: 'course1002'},
+      {id: 1003, name: 'course1003'},
+    ];
+  }
+
+  trackCourse(index, course) {
+    return course ? course.id : undefined;
+  }
 }
