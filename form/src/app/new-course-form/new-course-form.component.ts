06c156943250211383a04178329f37100b8fde19
Renee Thomsen <rcthomsen19@gmail.com>
2021-04-12 20:29:59 -0400

    formarray

--- form/src/app/new-course-form/new-course-form.component.ts
+++ form/src/app/new-course-form/new-course-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormGroup, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'new-course-form',
@@ -6,6 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-course-form.component.css']
 })
 export class NewCourseFormComponent implements OnInit {
+  form = new FormGroup({
+    topics: new FormArray([])
+  });
+
+  addTopic(topic: HTMLInputElement) {
+    this.topics.push(new FormControl(topic.value));
+    topic.value='';
+  }
+
+  removeTopic(topic: FormControl) {
+    let index = this.topics.controls.indexOf(topic);
+    this.topics.removeAt(index);
+  }
+  
+  get topics() {
+    return this.form.get('topics') as FormArray;
+  }
+
+
+
   newCourses = [
     {id:1, name: 'Java'},
     {id:2, name: 'Angular'},
